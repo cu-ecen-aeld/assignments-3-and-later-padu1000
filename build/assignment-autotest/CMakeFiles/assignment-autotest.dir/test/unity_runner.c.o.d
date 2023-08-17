a2dd6273fbd09fbeb8029c0255679385be3ea23b
Patrick <pat.durussel@gmail.com>
2023-08-17 14:07:08 +0200

    Deleting all

--- build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
+++
@@ -1,3 +0,0 @@
-assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
- /home/patrick/Documents/coursera/Linux/assignment-1-padu1000/assignment-autotest/test/unity_runner.c \
- /usr/include/stdc-predef.h
